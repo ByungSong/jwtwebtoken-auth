2f5f7964169e0b6f25ebb9617cc5600cebf4b08e
ByungSong <byung.song@codeimmersives.com>
2019-01-17 15:38:36 -0500

    ...

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.json({
-    "message":"Welcome Byung again!"
+    "message":"Welcome Byung!"
   });
 });
 
